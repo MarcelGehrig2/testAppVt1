91283f965c4b420afb9d141ece9f91a9def755fa
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-01-16 15:11:16 +0100

    hello world working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+// #include <b
+#include <eeros/logger/Logger.hpp>
+#include <eeros/logger/StreamLogWriter.hpp>
+ 
+int main() {
+	using namespace eeros::logger;
+ 
+	StreamLogWriter w(std::cout);
+	Logger<LogWriter>::setDefaultWriter(&w);
+	Logger<LogWriter> log;
+ 
+	log.info() << "Hello, EEROS";
+ 
+	return 0;
 }
---
+++ startScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# g++ main.cpp -o testAppVt1 -pthread -std=c++11
+
+cd ~/git_repos/VT1/build/testAppVt1
+
+make
+
+
+~/git_repos/VT1/build/testAppVt1/testappvt1
